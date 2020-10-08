3a93630cc434cf2434fa9e03cd5265443c94734e
Mychael M <mychaelbmenges@gmail.com>
2020-10-08 15:40:13 -0600

    added test for firstName input field

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,21 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
+import ContactForm from "./components/ContactForm";
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+test("user can fill out form and submit", () => {
+  render(<ContactForm />);
+
+  // query VDOM for the input field
+  const firstNameInput = screen.getByPlaceholderText(/firstName/i);
+  
+  // fill out input
+  fireEvent.change(firstNameInput, {target: {value: "Mychael"}});
+
+  // assert that input contains expected value
+  expect(firstNameInput).toHaveValue("Mychael")
+})
